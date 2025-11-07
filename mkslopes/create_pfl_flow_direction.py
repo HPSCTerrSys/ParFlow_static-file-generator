f2fa9aac2a13f56d93162ef17674cff233026d59
Marco van Hulten <Marco.van.Hulten@uni-bonn.de>
2025-11-07 09:59:15 +0100

    Calculate slopes based on unmodified DEM
    Burning and other modification must be done later.  When slopes are calculated
    based on burnt DEM, slope values are wrong (most would be zero).

--- mkslopes/create_pfl_flow_direction.py
+++ mkslopes/create_pfl_flow_direction.py
@@ -5,12 +5,9 @@
 from pysheds.grid import Grid
 
 # Read elevation raster
-grid_orig = Grid.from_raster('HSURF-Sphere.tiff')
-grid_burnt = Grid.from_raster('HSURFBurnedAndMod-Sphere.tiff')
-dem_orig = grid_orig.read_raster('HSURF-Sphere.tiff')
-dem_burnt = grid_burnt.read_raster('HSURFBurnedAndMod-Sphere.tiff')
+grid = Grid.from_raster('HSURF-Sphere.tiff')
+dem = grid.read_raster('HSURF-Sphere.tiff')
 
-#FIXME/TODO: NOT SURE IF THIS IS NEEDED FOR EITHER BURNT OR ORIG
 ## Fill pits and depressions and resolve flats in DEM
 #pit_filled_dem = grid.fill_pits(dem)
 #flooded_dem = grid.fill_depressions(pit_filled_dem)
@@ -19,17 +16,14 @@ dem_burnt = grid_burnt.read_raster('HSURFBurnedAndMod-Sphere.tiff')
 # Determine D8 flow directions from DEM
 dirmap = (64, 128, 1, 2, 4, 8, 16, 32)
 #fdir = grid.flowdir(inflated_dem, dirmap=dirmap)
-fdir_orig = grid_orig.flowdir(dem_orig, dirmap=dirmap)
-fdir_burnt = grid_burnt.flowdir(dem_burnt, dirmap=dirmap)
+fdir = grid.flowdir(dem, dirmap=dirmap)
 
 # Calculate the slopes
-slopes = grid_burnt.cell_slopes(dem_orig, fdir_orig)
-slopes = grid_burnt.cell_slopes(dem_orig, fdir_burnt)
-#FIXME: In both cases most of values become zero
+slopes = grid.cell_slopes(dem, fdir)
 
 # Write flow direction and slope fields to GeoTIFF
-grid_burnt.to_raster(data=fdir_burnt, file_name='flow_direction.tiff')
-grid_orig.to_raster(data=slopes, file_name='slopes.tiff')
+grid.to_raster(data=fdir, file_name='flow_direction.tiff')
+grid.to_raster(data=slopes, file_name='slopes.tiff')
 
 # Determine accumulation and write to GeoTIFF
 #acc = grid.accumulation(fdir, dirmap=dirmap)
