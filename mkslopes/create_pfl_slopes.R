8415a817f1012c97e821e148b0fcaa7750ce97cd
Marco van Hulten <Marco.van.Hulten@uni-bonn.de>
2026-02-26 15:51:30 +0100

    Call PriorityFlow::SlopeCalcUP()
    This should be consistent with the ParFlow overland flow boundary condition.

--- mkslopes/create_pfl_slopes.R
+++ mkslopes/create_pfl_slopes.R
@@ -1,11 +1,24 @@
 #!/usr/bin/env R
 # Create D4 ParFlow slope files
 
-# The 'remotes' package is to grab stuff from GitHub &co, but at JSC it is in R
-#install.packages('remotes')
+topo_nc <- paste0(args[1])
+
+# On JSC HPC 'remotes' is already included in the R module
+if (!requireNamespace("remotes", quietly=TRUE)) install.packages('remotes')
 library(remotes)
 install_github("lecondon/PriorityFlow", subdir="Rpkg")
 
+# Install packages that are dependencies but not described as such by PriorityFlow
 install.packages('fields')
 
+# Use the PriorityFlow package to calculate slopes
+# TODO: complete and correct arguments!
 library('PriorityFlow')
+PriorityFlow::SlopeCalcUP(
+            dem = topo_nc,
+            direction = , # e.g. flow_direction.{tiff,nc} from pysheds routine
+            dx = 0.11, dy = 0.11,
+            mask = "../mklandmask/EUR-11_TSMP_FZJ-IBG3_444x432_LAND-LAKE-SEA-MASK.nc",
+            borders = ,
+            rivermask = # e.g. RiverMask.nc created by burnShape2Topo.py
+)
